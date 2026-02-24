fa6a7b226d7d0e8b77966bffb68e622aebfd80db
neessam moustafa <neessam@pbc.guru>
2026-02-24 00:13:51 -0500

    formatting fixes

--- points2d.h
+++ points2d.h
@@ -126,8 +126,9 @@ class Points2D {
       out << "()" << std::endl;
     } else {
       for (size_t i = 0; i < some_points.size_; ++i) {
+        if (i > 0) out << " ";
         out << "(" << some_points.sequence_[i][0] << ", "
-            << some_points.sequence_[i][1] << ") ";
+            << some_points.sequence_[i][1] << ")";
       }
       out << std::endl;
     }
@@ -167,6 +168,7 @@ class Points2D {
       some_points.sequence_[i][1] = y;
     }
 
+    std::cout << std::endl;
     return in;
   }
 
